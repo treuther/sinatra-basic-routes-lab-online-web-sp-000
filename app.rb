58752570eeb3d2bb8e803fe377af768742791e14
Tracy Reuther <tracyreuther@gmail.com>
2019-09-02 10:27:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Tracy"
+  end
+
+  get '/hometown' do
+    "My hometown is Houston"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is I Love Rock n Roll by Joan Jett"
+  end
+
 end
